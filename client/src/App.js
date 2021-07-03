827fc3467b43132939c455fc8c3e05900cb695b7
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2021-07-03 13:52:43 -0400

    material-ui for toolbar used
    added a toolbar with basic features.

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+// import { Component } from 'react';
+//import Loginbar from './components/Loginbar';
+import React, {useState, useEffect } from 'react';
+import Loginbar from './components/MenuComps/Loginbar';
 
+function App (props) {
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.props = props;
-    this.state = {
-      apiResponse: ""
-    };
-  }
-  callAPI(){
-    fetch("http://localhost:9000/reactAPI")
-    .then(res => res.text())
-    .then(res => this.setState({ apiResponse: res }));
-  }
+  const [apiResponse, set_apiResponse] = useState();
 
-  componentDidMount(){
-      this.callAPI();
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <footer>
-          {this.state.apiResponse}
-        </footer>
-      </div>
-    );
-  }
+  useEffect(() => {
+    function callAPI() {
+      fetch("http://localhost:9000/reactAPI")
+        .then(res => res.text())
+          .then(res => set_apiResponse(res));
+    }
+
+    callAPI();
+
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        < Loginbar notify_count={14} mail_count={150} />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <footer>
+        {apiResponse}
+      </footer>
+    </div>
+  )
 }
 
 export default App;
---
+++ client/src/components/MenuComps/Mainbar.js
@@ -0,0 +1,37 @@
+// import { Toolbar, AppBar, IconButton, Typography, Button, Menu, makeStyles} from "@material-ui/core";
+// import MenuIcon from '@material-ui/icons/Menu';
+
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//       flexGrow: 1,
+//     },
+//     menuButton: {
+//       marginRight: theme.spacing(2),
+//     },
+//     title: {
+//       flexGrow: 1,
+//     },
+// }));
+  
+// function Mainbar() {
+//     const classes = useStyles();
+  
+//     return (
+//       <div>
+//         <AppBar className={classes.root} position="static">
+//           <Toolbar>
+//             <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+//               <MenuIcon />
+//             </IconButton>
+//             <Typography variant="h6" className={classes.title}>
+//               Quiztor
+//             </Typography>
+//             <Button color="inherit">Login</Button>
+//           </Toolbar>
+//         </AppBar>
+//       </div>
+//     );
+// }
+  
+
+// export default Mainbar;
