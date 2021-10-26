14bbadb0d3184060d155f3a3f3fab4c906d6ba27
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2021-10-26 12:43:40 -0400

    updated error message in UserModel Schema

--- api/schema/user.js
+++ api/schema/user.js
@@ -1,12 +1,27 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const {isEmail} = require('validator');
 const Schema = mongoose.Schema;
 
 // tokens for authentication purposes
 const userSchema = new Schema({
-    username : {type: String, unique: true},
-    email: {type: String},
-    password : {type: String},
-    token : {type: String},
+    username : {
+        type: String, 
+        required: [true, 'Please enter a username'] ,
+        unique: true
+    },
+    email: {
+        type: String, 
+        lowercase: true,
+        validator: [isEmail, 'Please enter a valid email']
+    },
+    password : {
+        type: String,
+        required: [true, 'Please enter a password'],
+        minlength: [2, 'Minimum password length is 2 characters']
+    },
+    token : {
+        type: String
+    },
 });
 
 // This will be the updated schema the mongoose database
